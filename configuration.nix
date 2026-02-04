25ebcfa652bd62defa69f671255baaf15755746a
Christa Clegg <cleggct@gmail.com>
2026-02-04 20:13:16 +0200

    removed unneeded import from configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -169,8 +169,6 @@
      niri
   ];
 
-  home-manager.users.cc = import ./home.nix;
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
