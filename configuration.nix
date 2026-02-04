156f69847d082b88afa464ea04451c60bc422ac9
Christa Clegg <cleggct@gmail.com>
2026-02-04 20:32:36 +0200

    enable niri in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -169,6 +169,9 @@
      niri
   ];
 
+  # Enable niri
+  programs.niri.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
