f228a4f558a07b381f2a5c07d2c47d8d78579e0b
Christa Clegg <cleggct@gmail.com>
2026-02-06 02:00:01 +0200

    iosevka terminal font

--- configuration.nix
+++ configuration.nix
@@ -162,8 +162,8 @@
       };
 
       monospace = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans Mono";
+        package = pkgs.iosevka;
+        name = "Iosevka";
       };
 
       emoji = {
