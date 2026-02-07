5f45e35c4abe01f0692e7c6fe3d2fb90c636b93c
Christa Clegg <cleggct@gmail.com>
2026-02-08 00:57:00 +0200

    set up niri with sodiboo flake

--- modules/home-manager/niri.nix
+++ modules/home-manager/niri.nix
@@ -1,4 +1,27 @@
 { pkgs, ... }:
+
+let
+  noctalia = cmd: [
+    "noctalia-shell" "ipc" "call"
+  ] ++ (pkgs.lib.splitString " " cmd);
+in
 {
-  
+  programs.niri = {
+    package = niri;
+    settings = {
+      binds = with config.lib.niri.actions; {
+        "Mod+L".action.spawn = noctalia "lockScreen lock";
+        "XF86AudioLowerVolume".action.spawn = noctalia "volume decrease";
+        "XF86AudioRaiseVolume".action.spawn = noctalia "volume increase";
+        "XF86AudioMute".action.spawn = noctalia "volume muteOutput";
+      };
+      spawn-at-startup = [
+        {
+          command = [
+            "noctalia-shell"
+          ];
+        }
+      ];
+    };
+  };
 }
